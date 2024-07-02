32c8a7b4a7ce897a1afdab723cc64b2e1c37ccde
Rocky <83218418+Rocky-pup@users.noreply.github.com>
2024-07-02 23:50:51 +0300

    Update config_sample.ts

--- config/config_sample.ts
+++ config/config_sample.ts
@@ -1,9 +1,5 @@
-const   
-    totalShards = "auto", // Total shards for your bot
-    shardsPerCluster = 10, // How much shards you wanna spawn per cluster
-    
+const
     bridge_authToken = "auth", // Password for discord-cross-hosting bridge
-    bridge_totalMachines = 1, // How much machines you'll use (check discord-cross-hosting docs) 
     bridge_host = "127.0.0.1", // ip of hosted bridge (localhost if it's hosted on this machine)
     bridge_port = 4444, // port of bridge
     bridge_use = false, // use bridge, or not? (Use only sharding if not. Won't affect anything cause `discord-cross-hosting` is still in dev.)
